ae7888a730c9b0a2373ae4770ee46b619e63108e
chennakesiBalaraju <87874918+chennakesiBalaraju@users.noreply.github.com>
2022-06-10 19:39:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const scene = new THREE.scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+); 
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(2,2,2 );
+const material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
+const cube = new THREE.Mesh( geometry, material);
+scene.add(car);
+
+camera.position.z = 5;
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.renderer(scene,camera);
+
+}
+animate();
